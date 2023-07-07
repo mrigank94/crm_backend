7831c28df4f0c7431142fc2bf69af0d5a1573376
Mrigank Kaushik <mriganknitw@gmail.com>
2023-07-07 19:39:15 +0530

    Deploying the app

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,4 +1,6 @@
 module.exports = {
-    DB_NAME: "crm_db",
-    DB_URL:  process.env.MONGODB_URI || "mongodb://localhost/crm_db"
-}
+  DB_NAME: "crm_db",
+  DB_URL:
+    process.env.MONGODB_URI ||
+    `mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@cluster0.qc4nvkx.mongodb.net/crm_db`,
+};
